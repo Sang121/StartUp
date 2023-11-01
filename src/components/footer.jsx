42130a7a59a7a0289f617dd02146ad19ae3d27f3
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-11-01 21:43:59 +0700

    edit footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,21 +5,21 @@ function Footer
   () {
   return (
     <div className='footer'>
-      
+
       <div className='line2'>
-      <Link to="https://www.facebook.com/Libra245" className='home-btn'>
-            <button type="submit" class=" btn  "><i class="fa fa-facebook" > Facebook</i></button>
-          </Link>
-          <Link to="https://www.instagram.com/imsn.02/" className='home-btn'>
-            <button type="submit" class=" btn "><i class="fa fa-instagram" > Instagram</i></button>
-          </Link>
+        <Link to="https://www.facebook.com/Libra245" className='home-btn'>
+          <button type="submit" class=" btn  "><i class="fa fa-facebook" > Facebook</i></button>
+        </Link>
+        <Link to="https://www.instagram.com/imsn.02/" className='home-btn'>
+          <button type="submit" class=" btn "><i class="fa fa-instagram" > Instagram</i></button>
+        </Link>
       </div>
       <div className='line3'>
         <ul>
-          <li>Địa chỉ:Quang trung,Yên Nghĩa, Thành phố Hà Nội, Việt Nam. Tổng đài hỗ trợ: 0345123230 - Email: 20010797@st.phenikaa-uni.edu.vn </li>
-          <li>Chịu Trách Nhiệm Quản Lý Nội Dung: Sáng - Điện thoại liên hệ: 0352797320 </li>
+          <li>Địa chỉ:Yên Nghĩa, Thành phố Hà Nội, Việt Nam. Tổng đài hỗ trợ: 0345123230 - Email: foremergency@gmail.com </li>
+          <li>Chịu Trách Nhiệm Quản Lý Nội Dung: HFP2 - Điện thoại liên hệ: 0352797320 </li>
           <li> </li>
-          <li>© 2023 - Bản quyền thuộc về Công ty TNHH 1 thành viên </li>
+          <li>© 2023 - Bản quyền thuộc về Công ty TNHH HFP2 </li>
         </ul></div>
     </div>
   )
